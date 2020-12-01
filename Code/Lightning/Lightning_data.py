9d20fbb4ac3cee6663708372791175c02b5860db
Jonas Mortelmans <70951991+MortelmansJonas@users.noreply.github.com>
2020-12-01 08:10:18 +0100

    Add files via upload

--- Code/Lightning/Lightning_data.py
+++ Code/Lightning/Lightning_data.py
@@ -1,12 +1,11 @@
 ## Import the necessary modules
 import pandas as pd
 import numpy as np
-from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-import netCDF4 as nc
 from netCDF4 import Dataset
+from datetime import datetime
 
 ## Read in the data
 data = pd.read_csv('/scratch/leuven/336/vsc33651/nu-wrf-dev/Great_Slave_Lake/SL_Short.csv', delimiter = ',')
@@ -15,7 +14,7 @@ print(data.head(5))
 print(data.columns)
 print(data.dtypes)
 
-data['Date'] = pd.to_datetime(data['Date'])
+data['Date'] = pd.to_datetime(data['Date']).astype(int)
 data['Time'] = pd.to_datetime(data['Time'])
 data['Type'] = data['Type'].astype('category')
 data['Latitude'] = round(data['Latitude'],2)
@@ -44,10 +43,13 @@ plt.show()
 flashes= data.groupby(['Date', 'Latitude', 'Longitude', 'Type']).agg({'Count': ['sum'],'EPC':['mean']}).reset_index()
 print(flashes)
 CCPL = flashes[flashes.Type == 'C'].reset_index()
-print(CCPL)
 CGPL = flashes[flashes.Type == 'G'].reset_index()
+del CCPL['index']
+del CGPL['index']
+CCPL.columns = ['Date', 'Latitude', 'Longitude', 'Type', 'Count', 'avg_EPC']
+CGPL.columns = ['Date', 'Latitude', 'Longitude', 'Type', 'Count', 'avg_EPC']
+print(CCPL)
 print(CGPL)
-
 ## Plot it
 plt.plot(CCPL.Date, CCPL['Count'], '.', label='CC')
 plt.plot(CGPL.Date, CGPL['Count'], '.', label='CG')
@@ -58,24 +60,6 @@ plt.legend()
 plt.show()
 
 ## Create a netCDF file
-# Method 1
-print('netCDF file: Method 1')
-df = data.to_xarray()
-print(df)
-df.to_netcdf('/scratch/leuven/336/vsc33651/nu-wrf-dev/Great_Slave_Lake/Slave_lake2.nc')
-
-# Doesn't give flashes/type -> can be split? (Doesn't work yet
-print('netCDF CC')
-CCPL.drop('index', axis=1)
-dCC = CCPL.to_xarray()
-dCC.to_netcdf('/scratch/leuven/336/vsc33651/nu-wrf-dev/Great_Slave_Lake/Slave_lake_CC.nc')
-
-print('netCDF CG')
-CGPL.drop('index', axis=1)
-dCG = CGPL.to_xarray()
-dCG.to_netcdf('/scratch/leuven/336/vsc33651/nu-wrf-dev/Great_Slave_Lake/Slave_lake_CG.nc')
-
-# Method 2 (Gives netCDF file with right variables, but empty)
 nf= '/scratch/leuven/336/vsc33651/nu-wrf-dev/Great_Slave_Lake/Slave_lake.nc'
 ds = Dataset(nf, 'w', format='NETCDF4')
 
@@ -88,26 +72,23 @@ lon = ds.createDimension('lon', None)
 times = ds.createVariable('time', 'f4', ('time',))
 lats = ds.createVariable('lat', float, ('lat',))
 lons = ds.createVariable('lon', float, ('lon',))
-Flashdensity_CC = ds.createVariable('Flashdensity_CC', int, ( 'time', 'lat', 'lon',), fill_value=-9999.0)
+Flashdensity_CC = ds.createVariable('Flashdensity_CC', int, ( 'time', 'lat', 'lon',), fill_value=0)
 Flashdensity_CC.units = 'Amount/day'
-Flashdensity_CG = ds.createVariable('Flashdensity_CG', int, ( 'time', 'lat', 'lon',), fill_value=-9999.0)
+Flashdensity_CG = ds.createVariable('Flashdensity_CG', int, ( 'time', 'lat', 'lon',), fill_value=0)
 Flashdensity_CG.units = 'Amount/day'
-EPC_CC = ds.createVariable('EPC_CC', float, ( 'time', 'lat', 'lon',), fill_value=-9999.0)
+EPC_CC = ds.createVariable('EPC_CC', float, ( 'time', 'lat', 'lon',), fill_value=0)
 EPC_CC.units = 'kA'
-EPC_CG = ds.createVariable('EPC_CG', float, ( 'time', 'lat', 'lon',), fill_value=-9999.0)
+EPC_CG = ds.createVariable('EPC_CG', float, ( 'time', 'lat', 'lon',), fill_value=0)
 EPC_CG.units = 'kA'
 
 # Create arrays of the necessary variables
-ta = np.array(CCPL['Date'])
-Ta = np.unique(ta) # To get each day only once
-Time_array = np.array(Ta)
+ta = np.unique(flashes['Date']) # To get each day only once
+Time_array = np.array(ta)
+times[:] = Time_array # Assign time values
 
 # Assign Latitude and Longitude Values
-lats[:] = np.array(np.unique(CCPL['Latitude']))
-lons[:] = np.array(np.unique(CCPL['Longitude']))
-
-# Assign time values
-times[:] = Time_array
+lats[:] = np.array(np.unique(flashes['Latitude']))
+lons[:] = np.array(np.unique(flashes['Longitude']))
 
 # Assign NETCDF Data Values
 for dim in ds.dimensions.values():
@@ -115,16 +96,47 @@ for dim in ds.dimensions.values():
 for var in ds.variables.values():
     print(var)
 
-Flashdensity_CC
-Flash_CC = []
-for i in range(0,len(times)):
-     for j in range(0,len(CCPL)):
-         if times[i] == CCPL['Date'][j]:
-             for k in range(0,len(lats)):
-                 if lats[k] == CCPL['Latitude'][j]:
-                     for m in range(0,len(lons)):
-                         if lons[m] == CCPL['Longitude'][j]:
+## Flashdensity_CC
+for i in range(0,len(Time_array)): # Loop through all the times
+     for j in range(0,len(CCPL)): # Loop through all the CCPL records
+         if Time_array[i] == CCPL['Date'][j]: # if the time in both arrays is the same, go to the following loop
+             for k in range(0,len(lats)): # Loop through all latitudes
+                 if lats[k] == CCPL['Latitude'][j]: # If the latitudes match, go to the next loop
+                     for m in range(0,len(lons)): # Loop through all longitudes
+                         if lons[m] == CCPL['Longitude'][j]: # If the longitudes match, put the CGPL record in Flashdensity_CG
                              Flashdensity_CC[i,k,m] = CCPL['Count'][j]
-                         Flash_CC.append(Flashdensity_CC)
-Flashdensity_CC = Flash_CC
 
+## Flashdensity_CG
+for i in range(0,len(Time_array)): # Loop through all the times
+     for j in range(0,len(CGPL)): # Loop through all the CGPL records
+         if Time_array[i] == CGPL['Date'][j]: # if the time in both arrays is the same, go to the following loop
+             for k in range(0,len(lats)): # Loop through all latitudes
+                 if lats[k] == CGPL['Latitude'][j]: # If the latitudes match, go to the next loop
+                     for m in range(0,len(lons)): # Loop through all longitudes
+                         if lons[m] == CGPL['Longitude'][j]: # If the longitudes match, put the CCPL record in EPC_CC
+                             Flashdensity_CG[i,k,m] = CGPL['Count'][j]
+
+## EPC_CC
+for i in range(0,len(Time_array)): # Loop through all the times
+     for j in range(0,len(CCPL)): # Loop through all the CCPL records
+         if Time_array[i] == CCPL['Date'][j]: # if the time in both arrays is the same, go to the following loop
+             for k in range(0,len(lats)): # Loop through all latitudes
+                 if lats[k] == CCPL['Latitude'][j]: # If the latitudes match, go to the next loop
+                     for m in range(0,len(lons)): # Loop through all longitudes
+                         if lons[m] == CCPL['Longitude'][j]: # If the longitudes match, put the CCPL record in EPC_CG
+                             Flashdensity_CG[i,k,m] = CCPL['avg_EPC'][j]
+
+## EPC_CG
+for i in range(0,len(Time_array)): # Loop through all the times
+     for j in range(0,len(CGPL)): # Loop through all the CGPL records
+         if Time_array[i] == CGPL['Date'][j]: # if the time in both arrays is the same, go to the following loop
+             for k in range(0,len(lats)): # Loop through all latitudes
+                 if lats[k] == CGPL['Latitude'][j]: # If the latitudes match, go to the next loop
+                     for m in range(0,len(lons)): # Loop through all longitudes
+                         if lons[m] == CGPL['Longitude'][j]: # If the longitudes match, put the CCPL record in Flashdensity_CC
+                             Flashdensity_CG[i,k,m] = CGPL['avg_EPC'][j]
+
+# For ease: convert times back to easy readable format
+TA = np.unique(pd.to_datetime(flashes['Date']))
+Time_array = np.array(TA)
+times[:] = Time_array # Assign time values
