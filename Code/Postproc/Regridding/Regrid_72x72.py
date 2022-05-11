a5ab8875efa24054d532d94fd2bb92449da006c3
Jonas Mortelmans <70951991+MortelmansJonas@users.noreply.github.com>
2022-05-11 11:26:01 +0200

    Update Regrid_72x72.py

--- Code/Postproc/Regridding/Regrid_72x72.py
+++ Code/Postproc/Regridding/Regrid_72x72.py
@@ -5,7 +5,7 @@
 infile_d01 = '/scratch/leuven/projects/lt1_2020_es_pilot/project_output/rsda/vsc33651/NU-WRF/wrfout_nc_files/data_calibrated_ax_d01.nc'
 outfile = '/scratch/leuven/projects/lt1_2020_es_pilot/project_output/rsda/vsc33651/NU-WRF/wrfout_nc_files/regrid_72x72.nc'
 
-LIs = ['LTG3', 'LPI', 'PR92W', 'CAPExP_R', 'CAPExP_CSI']
+LIs = ['LTG3', 'LPI', 'PR92W', 'CAPExP_R']
 
 import numpy as np
 import pandas as pd
@@ -35,12 +35,10 @@ trg.createVariable('LPI_d01', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('LTG3_d01', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('PR92W_d01', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('CAPExP_R_d01', 'f4', dimensions=('time','lat','lon',), zlib=True)
-trg.createVariable('CAPExP_CSI_d01', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('LPI_d02', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('LTG3_d02', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('PR92W_d02', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('CAPExP_R_d02', 'f4', dimensions=('time','lat','lon',), zlib=True)
-trg.createVariable('CAPExP_CSI_d02', 'f4', dimensions=('time','lat','lon',), zlib=True)
 trg.createVariable('Obs', 'f4', dimensions=('time','lat','lon',), zlib=True)
 
 for i in range(0,8):
@@ -50,13 +48,11 @@ for i in range(0,8):
         m = j*8
         n = (j+1)*8
         trg['CAPExP_R_d01'][:,i,j] = np.nanmean(ds01['CAPExP_R_d01'][:,k:l,m:n], axis=(1,2))
-        trg['CAPExP_CSI_d01'][:, i, j] = np.nanmean(ds01['CAPExP_CSI_d01'][:, k:l, m:n], axis=(1, 2))
         trg['LTG3_d01'][:, i, j] = np.nanmean(ds01['LTG3_d01'][:, k:l, m:n], axis=(1, 2))
         trg['LPI_d01'][:, i, j] = np.nanmean(ds01['LPI_d01'][:, k:l, m:n], axis=(1, 2))
         trg['PR92W_d01'][:, i, j] = np.nanmean(ds01['PR92W_d01'][:, k:l, m:n], axis=(1, 2))
         trg['Obs'][:, i, j] = np.nanmean(ds01['Obs'][:, k:l, m:n], axis=(1, 2))
         trg['CAPExP_R_d02'][:,i,j] = np.nanmean(ds01['CAPExP_R_d02'][:,k:l,m:n], axis=(1,2))
-        trg['CAPExP_CSI_d02'][:, i, j] = np.nanmean(ds01['CAPExP_CSI_d02'][:, k:l, m:n], axis=(1, 2))
         trg['LTG3_d02'][:, i, j] = np.nanmean(ds01['LTG3_d02'][:, k:l, m:n], axis=(1, 2))
         trg['LPI_d02'][:, i, j] = np.nanmean(ds01['LPI_d02'][:, k:l, m:n], axis=(1, 2))
         trg['PR92W_d02'][:, i, j] = np.nanmean(ds01['PR92W_d02'][:, k:l, m:n], axis=(1, 2))
